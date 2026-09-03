88f48d14c91f1333ef0f8e584c9ce321017c6ce2
Farheen Kaifee <farheenkaifee555@gmail.com>
2026-09-03 15:40:19 +0530

    Initial project setup

---
+++ backend/demoenv.js
@@ -0,0 +1,9 @@
+// PORT=5000
+// MONGO_URI=mongodb://127.0.0.1:27017/employee_management
+// JWT_SECRET=your_super_secret_key_change_later
+
+// PORT=5000
+
+// MONGO_URI=mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER>.mongodb.net/employee_management
+
+// JWT_SECRET=your_super_secret_key_change_later
